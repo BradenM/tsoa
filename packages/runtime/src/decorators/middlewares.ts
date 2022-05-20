ff97e77a7993f9db95c041aeb813915a68dc146a
Braden Mars <bradenmars@bradenmars.me>
2022-05-20 08:17:19 -0500

    feat(runtime): extract decorators from middleware to reflection utils

--- packages/runtime/src/decorators/middlewares.ts
+++ packages/runtime/src/decorators/middlewares.ts
@@ -1,33 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { decorator } from '../utils/reflection';
+
 type Middleware<T extends Function | Object> = T;
 
 const TSOA_MIDDLEWARES = Symbol('@tsoa:middlewares');
-
-/**
- * Helper function to create a decorator
- * that can act as a class and method decorator.
- * @param fn a callback function that accepts
- *           the subject of the decorator
- *           either the constructor or the
- *           method
- * @returns
- */
-function decorator(fn: (value: any) => void) {
-  return (...args: any[]) => {
-    // class decorator
-    if (args.length === 1) {
-      fn(args[0]);
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    } else if (args.length === 3 && args[2].value) {
-      // method decorator
-      const descriptor = args[2] as PropertyDescriptor;
-      if (descriptor.value) {
-        fn(descriptor.value);
-      }
-    }
-  };
-}
 
 /**
  * Install middlewares to the Controller or a specific method.
