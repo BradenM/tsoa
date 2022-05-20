e020f61ec6d2c2a437a8eda7a775eee844b8f5f0
Braden Mars <bradenmars@bradenmars.me>
2022-05-20 08:17:53 -0500

    feat(runtime): annotate targets decorated with Security with metadata

--- packages/runtime/src/decorators/security.ts
+++ packages/runtime/src/decorators/security.ts
@@ -1,3 +1,24 @@
+export const TSOA_SECURITY = Symbol('@tsoa:security');
+
+export interface SecurityMeta {
+  security: string | Record<string, string[]>;
+  scopes?: string[];
+  target: { name: string; target: any };
+}
+
+export function fetchSecurity(target?: any): SecurityMeta[] {
+  return Reflect.getMetadata(TSOA_SECURITY, target ?? Reflect) || [];
+}
+
+export function defineSecurity(meta: SecurityMeta, target?: any) {
+  const securities = fetchSecurity();
+  Reflect.defineMetadata(TSOA_SECURITY, [...securities, meta], Reflect);
+  if (target) {
+    const targetSec = fetchSecurity(target);
+    Reflect.defineMetadata(TSOA_SECURITY, [...targetSec, meta], target);
+  }
+}
+
 /**
  * Can be used to indicate that a method requires no security.
  */
@@ -10,8 +31,8 @@ export function NoSecurity(): Function {
 /**
  * @param {name} security name from securityDefinitions
  */
-export function Security(name: string | { [name: string]: string[] }, scopes?: string[]): Function {
-  return () => {
-    return;
+export function Security(name: string | { [name: string]: string[] }, scopes?: string[]): ClassDecorator & MethodDecorator {
+  return (target: Function) => {
+    defineSecurity({ security: name, target: { name: target.name, target }, scopes }, target);
   };
 }
